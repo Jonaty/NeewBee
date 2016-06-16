50c439a3e9adf3f3fe3ccabc3501a009427af949
Naty <ingh9404@outlook.com>
2016-06-16 04:34:54 -0500

    Se agrego la tabla amigos

---
+++ database/migrations/2016_06_16_093216_create_amigos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAmigosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('amigos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('usuario_id')->unsigned();
+            $table->integer('amigo_id')->unsigned();
+            $table->boolean('aceptacion');
+            $table->timestamps();
+
+            $table->foreign('usuario_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('amigos');
+    }
+}
