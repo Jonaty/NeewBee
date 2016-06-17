10360ae222ca9fd06a7ebb0a18fe9f8aae5c1cff
Naty <ingh9404@outlook.com>
2016-06-16 22:00:18 -0500

    Se agrego la tabla like

---
+++ database/migrations/2016_06_17_025728_create_like_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('usuario_id');
+            $table->integer('like_id');
+            $table->string('like_type');
+
+            $table->foreign('usuario_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('like');
+    }
+}
